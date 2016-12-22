fc29b4b50c2d75af5e016d3c168576c91ad05d99
romanchubak <romanchubak@gmail.com>
2016-12-22 23:02:33 +0200

    l

---
+++ lab6/Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TrackBar2 = New System.Windows.Forms.TrackBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TrackBar3 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar4 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar5 = New System.Windows.Forms.TrackBar()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar5, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(426, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(411, 97)
+        Me.TrackBar1.Maximum = 90
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(104, 45)
+        Me.TrackBar1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(442, 81)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "angel"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(442, 129)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(25, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "size"
+        '
+        'TrackBar2
+        '
+        Me.TrackBar2.Location = New System.Drawing.Point(411, 148)
+        Me.TrackBar2.Name = "TrackBar2"
+        Me.TrackBar2.Size = New System.Drawing.Size(104, 45)
+        Me.TrackBar2.TabIndex = 4
+        '
+        'Timer1
+        '
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(408, 180)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(15, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "R"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(407, 213)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(15, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "G"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(408, 244)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(14, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "B"
+        '
+        'TrackBar3
+        '
+        Me.TrackBar3.Location = New System.Drawing.Point(426, 181)
+        Me.TrackBar3.Maximum = 255
+        Me.TrackBar3.Name = "TrackBar3"
+        Me.TrackBar3.Size = New System.Drawing.Size(104, 45)
+        Me.TrackBar3.TabIndex = 8
+        '
+        'TrackBar4
+        '
+        Me.TrackBar4.Location = New System.Drawing.Point(426, 212)
+        Me.TrackBar4.Maximum = 255
+        Me.TrackBar4.Name = "TrackBar4"
+        Me.TrackBar4.Size = New System.Drawing.Size(104, 45)
+        Me.TrackBar4.TabIndex = 9
+        '
+        'TrackBar5
+        '
+        Me.TrackBar5.Location = New System.Drawing.Point(426, 243)
+        Me.TrackBar5.Maximum = 255
+        Me.TrackBar5.Name = "TrackBar5"
+        Me.TrackBar5.Size = New System.Drawing.Size(104, 45)
+        Me.TrackBar5.TabIndex = 10
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(426, 41)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(527, 288)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TrackBar5)
+        Me.Controls.Add(Me.TrackBar4)
+        Me.Controls.Add(Me.TrackBar3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TrackBar2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar5, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TrackBar2 As TrackBar
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TrackBar3 As TrackBar
+    Friend WithEvents TrackBar4 As TrackBar
+    Friend WithEvents TrackBar5 As TrackBar
+    Friend WithEvents Button2 As Button
+End Class
---
+++ lab6/Form1.vb
@@ -0,0 +1,146 @@
+﻿Public Class Form1
+
+    Dim Graph1 As Graphics
+    Dim brush3 As New SolidBrush(Color.Red)
+    Dim x As Integer, y As Integer, r, l As Integer
+    Dim x_senter, y_senter As Integer
+    Dim t As Integer
+    Dim rr, g, b As Integer
+    Dim gradus, radian As Double
+
+    Dim N As Integer = 0
+
+
+    Private Sub initialize()    'отже ця штука ініціалізує все в початкові умови
+        rr = 1 'це глобальна змінна кольору червоний
+        g = 1 'це глобальна змінна кольору зелений
+        b = 1   ' це глобальна змінна кольору голубий
+        Graph1 = Me.CreateGraphics() ' ініціалізація графіка там де можна малювати
+        Graph1.FillEllipse(New SolidBrush(Color.Black), 200, 15, 10, 10) 'малюємо точку зверху до якої кріпиться нитка
+        t = 0 ' функція коливань маятника залежить від т тому ми його глобально оголошуємо і кажем що на початку він дорівнює 0
+        gradus = TrackBar1.Value ' це змінна градус кута початкового від горизонтальної уявної прямої
+        radian = gradus * (Math.PI / 180) 'переводимо кут в радіани
+        l = 150 ' довжина нитки
+        r = TrackBar2.Value * 5 + 10 'це радіус того обєкта який коливається
+        x_senter = 210 ' координати того центра на якому прикріплена нитка
+        y_senter = 25
+        x = x_senter + l * Math.Cos(radian) 'вираховуємо координати положення обєкта маятника
+        y = y_senter + l * Math.Sin(radian)
+        'нище малюємо нитку, від ценрів віднімаємо аби поставити кінці нитки в ценри кіл
+        Graph1.DrawLine(New Pen(Color.Black), x_senter - 5, y_senter - 5, Convert.ToInt32(x), Convert.ToInt32(y))
+        'малюємо коло маятника заповнюючи його кольором нашим початковим тобто чорним і також зміщаємо його через особливість малювання еліпса в вб
+        Graph1.FillEllipse(New SolidBrush(Color.FromArgb(rr, g, b)), Convert.ToInt32(x - r / 2), Convert.ToInt32(y - r / 2), r, r)
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'кнопка старт ініціалізує і запускає тік в таймері
+        initialize()
+        Timer1.Enabled = True
+
+    End Sub
+
+
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'кнопка стоп зупиняє тік
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub TrackBar3_Scroll(sender As Object, e As EventArgs) Handles TrackBar3.Scroll
+        'повзунок для червоного кольору перемальовує все аналогічно ініціалізації
+        rr = TrackBar3.Value
+        Graph1.Clear(Color.White)
+        Graph1.FillEllipse(New SolidBrush(Color.Black), 200, 15, 10, 10)
+        gradus = TrackBar1.Value
+        radian = gradus * (Math.PI / 180)
+        r = TrackBar2.Value * 5 + 10
+        x = x_senter + l * Math.Cos(radian)
+        y = y_senter + l * Math.Sin(radian)
+        Graph1.DrawLine(New Pen(Color.Black), x_senter - 5, y_senter - 5, Convert.ToInt32(x), Convert.ToInt32(y))
+        Graph1.FillEllipse(New SolidBrush(Color.FromArgb(rr, g, b)), Convert.ToInt32(x - r / 2), Convert.ToInt32(y - r / 2), r, r)
+    End Sub
+
+    Private Sub TrackBar4_Scroll(sender As Object, e As EventArgs) Handles TrackBar4.Scroll
+        'повзунок для зеленого кольору перемальовує все аналогічно ініціалізації
+        g = TrackBar4.Value
+        Graph1.Clear(Color.White)
+        Graph1.FillEllipse(New SolidBrush(Color.Black), 200, 15, 10, 10)
+        gradus = TrackBar1.Value
+        radian = gradus * (Math.PI / 180)
+        r = TrackBar2.Value * 5 + 10
+        x = x_senter + l * Math.Cos(radian)
+        y = y_senter + l * Math.Sin(radian)
+        Graph1.DrawLine(New Pen(Color.Black), x_senter - 5, y_senter - 5, Convert.ToInt32(x), Convert.ToInt32(y))
+        Graph1.FillEllipse(New SolidBrush(Color.FromArgb(rr, g, b)), Convert.ToInt32(x - r / 2), Convert.ToInt32(y - r / 2), r, r)
+    End Sub
+    Private Sub TrackBar5_Scroll(sender As Object, e As EventArgs) Handles TrackBar5.Scroll
+        'повзунок для голубого кольору перемальовує все аналогічно ініціалізації
+        b = TrackBar5.Value
+        Graph1.Clear(Color.White)
+        Graph1.FillEllipse(New SolidBrush(Color.Black), 200, 15, 10, 10)
+        gradus = TrackBar1.Value
+        radian = gradus * (Math.PI / 180)
+        r = TrackBar2.Value * 5 + 10
+        x = x_senter + l * Math.Cos(radian)
+        y = y_senter + l * Math.Sin(radian)
+        Graph1.DrawLine(New Pen(Color.Black), x_senter - 5, y_senter - 5, Convert.ToInt32(x), Convert.ToInt32(y))
+        Graph1.FillEllipse(New SolidBrush(Color.FromArgb(rr, g, b)), Convert.ToInt32(x - r / 2), Convert.ToInt32(y - r / 2), r, r)
+    End Sub
+    Private Sub TrackBar2_Scroll(sender As Object, e As EventArgs) Handles TrackBar2.Scroll
+        'повзунок для зміни розміру обєкта перемальовує все аналогічно ініціалізації
+        initialize()
+        Graph1.Clear(Color.White)
+        Graph1.FillEllipse(New SolidBrush(Color.Black), 200, 15, 10, 10)
+        gradus = TrackBar1.Value
+        radian = gradus * (Math.PI / 180)
+        r = TrackBar2.Value * 5 + 10
+        x = x_senter + l * Math.Cos(radian)
+        y = y_senter + l * Math.Sin(radian)
+        Graph1.DrawLine(New Pen(Color.Black), x_senter - 5, y_senter - 5, Convert.ToInt32(x), Convert.ToInt32(y))
+        Graph1.FillEllipse(New SolidBrush(Color.FromArgb(rr, g, b)), Convert.ToInt32(x - r / 2), Convert.ToInt32(y - r / 2), r, r)
+    End Sub
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        'повзунок для зміни початкового кута перемальовує все аналогічно ініціалізації
+        initialize()
+        Graph1.Clear(Color.White)
+        Graph1.FillEllipse(New SolidBrush(Color.Black), 200, 15, 10, 10)
+        gradus = TrackBar1.Value
+        r = TrackBar2.Value * 5 + 10
+        radian = gradus * (Math.PI / 180)
+        x = x_senter + l * Math.Cos(radian)
+        y = y_senter + l * Math.Sin(radian)
+        Graph1.DrawLine(New Pen(Color.Black), x_senter - 5, y_senter - 5, Convert.ToInt32(x), Convert.ToInt32(y))
+        Graph1.FillEllipse(New SolidBrush(Color.FromArgb(rr, g, b)), Convert.ToInt32(x - r / 2), Convert.ToInt32(y - r / 2), r, r)
+
+
+    End Sub
+    ' ця вункція яка каже де обєкт який коливається в певний момент часу бере в себе час і початковий кут
+    Public Function angel(t As Integer, ang As Integer)
+        'це повністю правильна з точки зору фізики функція 
+        '        це поч кут в радіанах     е в степені -0.01 це коф затухання помн на час і понж на корінь прискор земне на довжину нитки
+        Return ((90 - ang) * (Math.PI / 180)) * Math.Pow(Math.E, 0 - 0.01 * t) * Math.Cos(Math.Sqrt(9.8 / l) * t)
+    End Function
+
+    'ну і це тік який робить анімацію все перемальовує
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Graph1.Clear(Color.White)   'затераємо все білим
+        Graph1.FillEllipse(New SolidBrush(Color.Black), 200, 15, 10, 10) 'точка опори зверху
+
+        Dim a As Double
+        'та функція розрахунку вертає кут в радіанах його не потрібно міняти для косінуса
+        a = angel(t, gradus)
+        t += 1 'збільшуємо наш час
+        x = x_senter + l * Math.Sin(a) 'перемальовуємо обєкт коливання і дальше все як вже було пояснено
+        y = y_senter + l * Math.Cos(a)
+        Graph1.DrawLine(New Pen(Color.Black), x_senter - 5, y_senter - 5, Convert.ToInt32(x), Convert.ToInt32(y))
+        Graph1.FillEllipse(New SolidBrush(Color.FromArgb(TrackBar3.Value, TrackBar4.Value, TrackBar5.Value)), Convert.ToInt32(x - r / 2), Convert.ToInt32(y - r / 2), r, r)
+
+    End Sub
+
+
+    End Class
